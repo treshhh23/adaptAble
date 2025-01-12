ea7ae9426585847ccf6432d96e454efa76f4fbf5
lin422-compeng <lin422@mcmaster.ca>
2025-01-11 22:32:17 -0500

    Finish all 3 buttons!!!!!!

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@ document.getElementById('high-contrast').addEventListener('click', () => {
     });
   });
   
+  document.getElementById("font-type").addEventListener('click', () => {
+    // Query the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      const activeTab = tabs[0];
+      // Send a message to content.js in the active tab
+      chrome.tabs.sendMessage(activeTab.id, {action: "toggleReadableFont"}, (response) => {
+        console.log(response.status);
+      });
+    });
+  });
+
+  document.getElementById("browser-zoom").addEventListener('click', () => {
+    // Query the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      const activeTab = tabs[0];
+      // Send a message to content.js in the active tab
+      chrome.tabs.sendMessage(activeTab.id, {action: "toggleZoom"}, (response) => {
+        console.log(response.status);
+      });
+    });
+  });
+
+
+  
